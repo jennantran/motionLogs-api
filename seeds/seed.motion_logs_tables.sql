174d4b1256ac7206e91df5e7812d3a540bcf5504
Jennifer Tran <jennantran@gmail.com>
2020-08-13 14:33:28 -0700

    add seed file

--- seeds/seed.motion_logs_tables.sql
+++ seeds/seed.motion_logs_tables.sql
@@ -7,9 +7,9 @@ TRUNCATE
 
 INSERT INTO log_users (username, password)
 VALUES
-    ('jennantran','password1'),
-    ('viethuynh','password2'),
-    ('jillyn','password3');
+    ('jennantran','$2a$12$bfT51PX60dNdSXkX3nRTzOdpThGOlmfG6Jwba11wqEWWdJAIAQ9i2'),
+    ('viethuynh','$2a$12$SroD5sKTrLbBXnQKjOVzTO2iKLR0/i71HILoDSBYaEheIE57esFy'),
+    ('jillyn','$2a$12$UVW62jOFLw/Eu1MXIjfk4OKabq424J7pO85aY.guR1oBx8WcIKYqy');
 
 
 INSERT INTO save_logs (log_id, wout_name, set, rep, weight, user_id, date_added)
