6fcd0b3b44fef3fff7fe72db00451e84602e0db0
Jennifer Tran <jennantran@gmail.com>
2020-08-13 17:20:28 -0700

    ready for db deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/motions',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/motions',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   }
