a369e39d503ade57f5f97319cf0a7266b08d26d8
Jennifer Tran <jennantran@gmail.com>
2021-08-04 15:55:41 -0500

    fix cors

--- src/app.js
+++ src/app.js
@@ -27,6 +27,12 @@ const morganOption = (NODE_ENV === 'production')
 //     res.send('All logs');
 // })
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(morgan(morganOption));
 app.use(cors())
 app.use(helmet());
