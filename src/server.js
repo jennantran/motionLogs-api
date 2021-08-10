c0d6c4c9527b4fa6ff34530262aaac9fe5c14d4a
Jennifer Tran <jennantran@gmail.com>
2021-08-10 17:56:08 -0500

    update ssl

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ const { PORT, DATABASE_URL } = require('./config');
   const db = knex({
     client: 'pg',
     connection: DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: true,
+    },
   });
 
   app.set('db', db);
