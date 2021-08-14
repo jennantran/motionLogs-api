9574eae043d96cd64d29fc8880a9bfd5ea249e37
Jennifer Tran <jennantran@gmail.com>
2021-08-14 17:12:18 -0500

    get rid of timeout

--- src/app.js
+++ src/app.js
@@ -9,17 +9,11 @@ const usersRouter = require('./users/users-router');
 const errorHandler = require('./error-handler');
 const authRouter = require('./auth/auth-router');
 const app = express();
-const cookieParser = require('cookie-parser');
-var timeout = require('connect-timeout')
+
 
 app.use(cors());
 
-app.use(timeout('5s'))
-app.use(cookieParser());
-app.use(haltOnTimedout)
 
-
-app.use(haltOnTimedout)
 
 app.get('/', (req, res) => {
     res.cookie('myCookie','express').send('Cookie set');
@@ -29,29 +23,19 @@ const morganOption = (NODE_ENV === 'production')
 ? 'tiny'
 : 'common';
 
-// app.get('/logs', (req, res, next) => {
-//     res.send('All logs');
-// })
+app.get('/logs', (req, res, next) => {
+    res.send('All logs');
+})
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://sheltered-fortress-79630.herokuapp.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-app.use(haltOnTimedout)
 
 app.use(morgan(morganOption));
-app.use(haltOnTimedout)
+
 app.use(helmet());
-app.use(haltOnTimedout)
+
 
 app.use('/api/logs', logsRouter);
 app.use('/api/signup', usersRouter);
 app.use('/api/auth', authRouter);
 
-// app.use(errorHandler);
-function haltOnTimedout (req, res, next) {
-    if (!req.timedout) next()
-  }
 
 module.exports = app;
--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -2,15 +2,9 @@ const express = require('express');
 const AuthService = require('./auth-service');
 const authRouter = express.Router();
 const jsonBodyParser = express.json();
-var timeout = require('connect-timeout')
 
   authRouter
     .post('/login', jsonBodyParser, (req, res, next) => {
-      savePost(req.body, function (err, id) {
-        if (err) return next(err)
-        if (req.timedout) return
-        res.send('saved as id ' + id)
-      })
       
       const { username, password } = req.body;
       const loginUser = { username, password };
@@ -20,16 +14,6 @@ var timeout = require('connect-timeout')
           return res.status(400).json({
             error: `Missing '${key}' in request body`
     });
-    function haltOnTimedout (req, res, next) {
-      if (!req.timedout) next()
-    }
-    
-    function savePost (post, cb) {
-      setTimeout(function () {
-        cb(null, ((Math.random() * 40000) >>> 0))
-      }, (Math.random() * 7000) >>> 0)
-    }
-    
 
     AuthService.getUserWithUserName(
        req.app.get('db'),
